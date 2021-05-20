2219fe19cbcb5b31936f1bc266356246688d25d4
samcasas <samycasas1@gmail.com>
2021-05-20 08:45:02 -0600

    se termina proyecto de stack mean

--- mean-stack-crud-app/src/app/app.module.ts
+++ mean-stack-crud-app/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './service/api.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
 import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+ 
 
 @NgModule({
   declarations: [
@@ -14,9 +19,13 @@ import { EmployeeListComponent } from './components/employee-list/employee-list.
     EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
